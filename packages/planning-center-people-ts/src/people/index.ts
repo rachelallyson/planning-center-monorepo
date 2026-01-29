3fdfbda175b07df2b679194c96f784e6d50bfaab
Rachel Allyson <me@rachelhigley.com>
2026-01-28 23:52:43 -0500

    chore(people): remove old core/people code and tests
    - Remove src/core/http.ts, src/core/pagination.ts (replaced by base)
    - Remove src/people/* (function-style API; use client modules)
    - Remove tests for removed code (core/http, core/pagination, people/*, integration people-*)

--- packages/planning-center-people-ts/src/people/index.ts
+++
@@ -1,9 +0,0 @@
-// Re-export all people-related functions from their respective modules
-export * from './core';
-export * from './contacts';
-export * from './fields';
-export * from './households';
-export * from './lists';
-export * from './notes';
-export * from './workflows';
-export * from './organization';
